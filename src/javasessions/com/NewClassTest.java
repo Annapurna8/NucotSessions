0b121eea94902eed35d45817a4c5ad37186764c3
Annapurna8 <lakkashetti17@gmail.com>
2023-11-17 13:50:38 +0530

    Second Prog

---
+++ src/javasessions/com/NewClassTest.java
@@ -0,0 +1,10 @@
+package javasessions.com;
+
+public class NewClassTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("New test program");
+	}
+
+}
