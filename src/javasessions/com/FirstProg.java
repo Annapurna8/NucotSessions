2c2424d8a0370ac52ebec638b4c2baa6a0ed5730
Annapurna8 <lakkashetti17@gmail.com>
2023-11-17 12:40:09 +0530

    first commit

---
+++ src/javasessions/com/FirstProg.java
@@ -0,0 +1,10 @@
+package javasessions.com;
+
+public class FirstProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("First Program");
+	}
+
+}
